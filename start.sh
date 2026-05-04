e7bc672877ab96bdd45a1b657ab5d84aea129bf0
OGL Partners <bulut@ogl.partners>
2026-05-04 09:03:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ echo "=== Running startup script ==="
 bash /link_models.sh
 
 echo "=== Starting ComfyUI worker ==="
+exec /start.sh.bak 2>/dev/null || \
+exec python -u /handler.py 2>/dev/null || \
 exec python -u /start.py
